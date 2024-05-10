3333681e1481630e8b3e9b0fd5d953484610985d
axthoison <axthoison@gmail.com>
2024-05-10 12:14:28 +0300

    added temporary desiggn to home page, created function to display movies from database and acces the details with the showModal function

--- website/base/views.py
+++ website/base/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from .models import Movie
 
 
 @login_required
 def home(request):
- return render(request, "home.html", {})
+ movies = Movie.objects.all()
+ context = {
+        'movies': movies,
+    }
+ return render(request, "home.html",context)
 
 
 def authView(request):
